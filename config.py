1e868688ff610fc9fea65ee3982936b9e975279f
jeon99yu <jeongkyu512@gmail.com>
2025-08-20 14:28:06 +0900

    feat: 환경변수관리 config파일

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = os.getenv("DB_NAME", "reviewdb")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
