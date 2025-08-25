e07cd53b79ef5aff6f7aed5a5d75c8c923bc3d02
jeon99yu <jeongkyu512@gmail.com>
2025-08-25 11:21:06 +0900

    setting: 보안관련

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ import sqlalchemy
 
 load_dotenv()
 
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_USER = os.getenv("DB_USER", "jeon99yu")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "test1234")
 DB_NAME = os.getenv("DB_NAME", "musinsa")
 DB_PORT = int(os.getenv("DB_PORT", 3306))
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 engine = sqlalchemy.create_engine(
     f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
